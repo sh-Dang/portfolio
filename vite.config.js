8faaf69c8aaaa459769e25588d330db875e4bf21
sh-Dang <henrys200632@gmail.com>
2025-11-21 20:56:18 +0900

    레포지토이 매핑

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lee-portfolio/', // 저장소 이름과 일치시키기
+  base: '/portfolio/', // 저장소 이름과 일치시키기
 })

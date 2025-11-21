552caab0a464cc8fa472f8be4d7922c89b7a72d7
sh-Dang <henrys200632@gmail.com>
2025-11-21 21:09:21 +0900

    라우터 기본설정 추가

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
